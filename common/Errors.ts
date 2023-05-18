7c0b2824fca07df23e6daa3e6981a6d76584a978
guorw <1215884602@qq.com>
2023-05-18 14:58:28 +0800

    tidy Errors

--- common/Errors.ts
+++ common/Errors.ts
@@ -1,21 +1,22 @@
 //contract specific errors
-export const WL_INVALID_PERMISSIONS = '50';
+export const WL_INVALID_PERMISSIONS = '300';
 
-export const DF_INVALID_ADDRESS = '51';
-export const DF_ALREADY_MINT = '52';
-export const DF_MUST_BE_WHITELISTED = '53';
-export const DF_PUBLIC_SALE_NOT_OPEN = '54';
-export const DF_MINT_PRICE_ERROR = '55';
-export const DF_MINT_QUANTITY_EXCEEDED = '56';
-export const DF_MAX_SUPPLY_EXCEEDED = '57';
-export const DF_INVALID_POOL_ADDRESS = '58';
-export const DF_STFIL_POOL_CALL_FAILED = '59';
-export const DF_WRONG_PERCENTAGE_RANGE = '60';
+export const DF_INVALID_ADDRESS = '301';
+export const DF_ALREADY_MINT = '302';
+export const DF_MUST_BE_WHITELISTED = '303';
+export const DF_PUBLIC_SALE_NOT_OPEN = '304';
+export const DF_MINT_PRICE_ERROR = '305';
+export const DF_MINT_QUANTITY_EXCEEDED = '306';
+export const DF_MAX_SUPPLY_EXCEEDED = '307';
+export const DF_INVALID_POOL_ADDRESS = '308';
+export const DF_STFIL_POOL_CALL_FAILED = '309';
+export const DF_WRONG_PERCENTAGE_RANGE = '310';
+export const DF_MIN_POWER_MORE_HALF_MAX_POWER = '311';
 
-export const RS_INVALID_ADDRESS = '61';
-export const RS_REFERRAL_CODE_MUST_BE_IN_THE_RANGE = '62';
-export const RS_REFERRAL_CODE_ALREADY_EXIST = '63';
-export const RS_REFERRAL_CODE_NUM_LIMITED = '64';
+export const RS_INVALID_ADDRESS = '312';
+export const RS_REFERRAL_CODE_MUST_BE_IN_THE_RANGE = '313';
+export const RS_REFERRAL_CODE_ALREADY_EXIST = '314';
+export const RS_REFERRAL_CODE_NUM_LIMITED = '315';
 
-export const MATH_MULTIPLICATION_OVERFLOW = '65';
-export const MATH_DIVISION_BY_ZERO = '66';
+export const MATH_MULTIPLICATION_OVERFLOW = '316';
+export const MATH_DIVISION_BY_ZERO = '317';
--- contracts/protocol/libraries/helpers/Errors.sol
+++ contracts/protocol/libraries/helpers/Errors.sol
@@ -13,25 +13,25 @@ pragma solidity ^0.8.0;
  */
 library Errors {
   //contract specific errors
-  string public constant WL_INVALID_PERMISSIONS = '50';
+  string public constant WL_INVALID_PERMISSIONS = '300';
 
-  string public constant DF_INVALID_ADDRESS = '51';
-  string public constant DF_ALREADY_MINT = '52';
-  string public constant DF_MUST_BE_WHITELISTED = '53';
-  string public constant DF_PUBLIC_SALE_NOT_OPEN = '54';
-  string public constant DF_MINT_PRICE_ERROR = '55';
-  string public constant DF_MINT_QUANTITY_EXCEEDED = '56';
-  string public constant DF_MAX_SUPPLY_EXCEEDED = '57';
-  string public constant DF_INVALID_POOL_ADDRESS = '58';
-  string public constant DF_STFIL_POOL_CALL_FAILED = '59';
-  string public constant DF_WRONG_PERCENTAGE_RANGE = '60';
-  string public constant DF_MIN_POWER_MORE_HALF_MAX_POWER = '61';
+  string public constant DF_INVALID_ADDRESS = '301';
+  string public constant DF_ALREADY_MINT = '302';
+  string public constant DF_MUST_BE_WHITELISTED = '303';
+  string public constant DF_PUBLIC_SALE_NOT_OPEN = '304';
+  string public constant DF_MINT_PRICE_ERROR = '305';
+  string public constant DF_MINT_QUANTITY_EXCEEDED = '306';
+  string public constant DF_MAX_SUPPLY_EXCEEDED = '307';
+  string public constant DF_INVALID_POOL_ADDRESS = '308';
+  string public constant DF_STFIL_POOL_CALL_FAILED = '309';
+  string public constant DF_WRONG_PERCENTAGE_RANGE = '310';
+  string public constant DF_MIN_POWER_MORE_HALF_MAX_POWER = '311';
 
-  string public constant RS_INVALID_ADDRESS = '62';
-  string public constant RS_REFERRAL_CODE_MUST_BE_IN_THE_RANGE = '63';
-  string public constant RS_REFERRAL_CODE_ALREADY_EXIST = '64';
-  string public constant RS_REFERRAL_CODE_NUM_LIMITED = '65';
+  string public constant RS_INVALID_ADDRESS = '312';
+  string public constant RS_REFERRAL_CODE_MUST_BE_IN_THE_RANGE = '313';
+  string public constant RS_REFERRAL_CODE_ALREADY_EXIST = '314';
+  string public constant RS_REFERRAL_CODE_NUM_LIMITED = '315';
 
-  string public constant MATH_MULTIPLICATION_OVERFLOW = '66';
-  string public constant MATH_DIVISION_BY_ZERO = '67';
+  string public constant MATH_MULTIPLICATION_OVERFLOW = '316';
+  string public constant MATH_DIVISION_BY_ZERO = '317';
 }
