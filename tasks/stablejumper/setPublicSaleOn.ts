9d20d068ca705d60ee5c7e1518ff3e3a3aa31e50
guorw <1215884602@qq.com>
2023-05-23 15:42:37 +0800

    fix set-public-sale-on task

--- tasks/stablejumper/setPublicSaleOn.ts
+++ tasks/stablejumper/setPublicSaleOn.ts
@@ -12,7 +12,7 @@ task("set-public-sale-on", "Set public sale on")
         const deploymentFilename = getDeploymentFilename(hre.network.name)
         const deployments = <IDeployments>getDeploymentFile(deploymentFilename);
         
-        const StableJumperAddress = <StableJumper>await hre.ethers.getContractAt("StableJumper", deployments.StableJumper)
+        const StableJumperAddress = <StableJumper>await hre.ethers.getContractAt("StableJumper", deployments.stableJumper)
         const tx = await StableJumperAddress.setPublicSaleOn(params.state)
         await tx.wait()
         console.log(`Successfully change whitelist merkle root
